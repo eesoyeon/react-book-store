ddb12d2bf31c16929ca9c7746cce9ab1c2f3ae67
Soyeon Lee <pink731p@gmail.com>
2024-11-13 01:49:50 +0900

    feat: 헤더, 푸터 컴포넌트 생성

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,10 +1,40 @@
+import styled from 'styled-components';
+
 function Footer() {
     return (
-        <>
-            <hr />
-            copyright(c), 2024, book store.
-        </>
+        <FooterStyle>
+            <h1 className="logo">
+                <img alt="book store" />
+            </h1>
+            <div>
+                <p>copyright(c), 2024, book store.</p>
+            </div>
+        </FooterStyle>
     );
 }
+
+const FooterStyle = styled.header`
+    width: 100%;
+    margin: 0 auto:
+    max-width: ${({ theme }) => theme.layout.width.large};
+    padding: 20px 0;
+    border-top: 1px solid ${({ theme }) => theme.color.background};
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .logo {
+        img {
+            witdh: 140px;
+        }
+    }
+    .copyright {
+        p {
+            font-size: 0.75rem;
+            color: ${({ theme }) => theme.color.text};
+        }
+    }
+    
+
+`;
 
 export default Footer;
