1be9f4d5fcea25ae1ed5032dbe3e040e80513ba3
Soyeon Lee <pink731p@gmail.com>
2024-11-13 01:48:51 +0900

    feat: 제목 컴포넌트 테스트

---
+++ src/components/common/Title.spec.tsx
@@ -0,0 +1,44 @@
+import { render, screen } from '@testing-library/react';
+
+import Title from './Title';
+import { BookStoreThemeProvider } from '../../context/themeContext';
+
+describe('Title 컴포넌트 테스트', () => {
+    it('렌더를 확인', () => {
+        // 1 렌더
+        render(
+            <BookStoreThemeProvider>
+                <Title size="large">제목</Title>
+            </BookStoreThemeProvider>
+        );
+
+        // 2 확인
+        expect(screen.getByText('제목')).toBeInTheDocument(); // 제목을 가져와서, 화면상에 있는지 확인
+    });
+
+    it('size props 적용', () => {
+        // 1 렌더
+        const { container } = render(
+            <BookStoreThemeProvider>
+                <Title size="large">제목</Title>
+            </BookStoreThemeProvider>
+        );
+
+        // 2 확인
+        expect(container?.firstChild).toHaveStyle({ fontSize: '2rem' }); // 제목을 가져와서, 화면상에 있는지 확인
+    });
+
+    it('color props 적용', () => {
+        // 1 렌더
+        const { container } = render(
+            <BookStoreThemeProvider>
+                <Title size="large" color="primary">
+                    제목
+                </Title>
+            </BookStoreThemeProvider>
+        );
+
+        // 2 확인
+        expect(container?.firstChild).toHaveStyle({ color: 'brown' }); // 제목을 가져와서, 화면상에 있는지 확인
+    });
+});
